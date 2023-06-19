b65d2bf056a184e2ec6a707d4e45dae6b8ee92b4
Hui_Zhang <1187896+yiaimer@users.noreply.github.com>
2023-06-18 23:25:22 -0500

    16-tweets-retrieve-comments

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,6 +1,7 @@
 from accounts.api.serializers import UserSerializer,UserSerializerForTweet
 from rest_framework import serializers
 from tweets.models import Tweet
+from comments.api.serializers import CommentSerializer
 
 class TweetSerializer(serializers.ModelSerializer):
     user = UserSerializerForTweet()
@@ -22,3 +23,12 @@ class TweetCreateSerializer(serializers.ModelSerializer):
         content = validated_data['content']
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
+
+class TweetSerializerWithComments(serializers.ModelSerializer):
+    user = UserSerializer()
+    # <HOMEWORK> 使用 serialziers.SerializerMethodField 的方式实现 comments
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'comments', 'created_at', 'content')
