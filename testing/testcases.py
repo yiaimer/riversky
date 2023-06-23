46b67f0bc07cdd09c3a2ebd8164ad4261930f1f2
Hui_Zhang <1187896+yiaimer@users.noreply.github.com>
2023-06-22 23:50:26 -0500

    20-inject-likes-info-to-other-apis

--- testing/testcases.py
+++ testing/testcases.py
@@ -5,6 +5,7 @@ from rest_framework.test import APIClient
 from tweets.models import Tweet
 from django.contrib.contenttypes.models import ContentType
 from likes.models import Like
+from newsfeeds.models import NewsFeed
 
 
 class TestCase(DjangoTestCase):
@@ -30,6 +31,9 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
+    def create_newsfeed(self, user, tweet):
+        return NewsFeed.objects.create(user=user, tweet=tweet)
 
     def create_comment(self, user, tweet, content=None):
         if content is None:
