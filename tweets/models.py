da566156e23bc416c70fd36777df426e0a3b2262
Hui_Zhang <1187896+yiaimer@users.noreply.github.com>
2023-08-24 23:33:21 -0500

    33-tweet-cache

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,11 @@
-from accounts.services import UserService
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save
 from likes.models import Like
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
+from utils.listeners import invalidate_object_cache
+from utils.memcached_helper import MemcachedHelper
 from utils.time_helpers import utc_now
 
 class Tweet(models.Model):
@@ -37,7 +39,7 @@ class Tweet(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
 
 class TweetPhoto(models.Model):
     # 图片在哪个 Tweet 下面
@@ -77,3 +79,4 @@ class TweetPhoto(models.Model):
     def __str__(self):
         return f'{self.tweet_id}: {self.file}'
 
+post_save.connect(invalidate_object_cache, sender=Tweet)
